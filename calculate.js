beabc3e02178c6e6341bd9400666aeda4f13613c
rahul p <rapatil@ccrn.com>
2023-01-27 15:41:19 +0530

    initial commit

---
+++ calculate.js
@@ -0,0 +1,4 @@
+const myCalculator = require("my-calculator");
+const { add, subtract, multiply, divide } = myCalculator;
+
+console.log(add(2,5))
---
+++ node_modules/my-calculator/test.js
@@ -0,0 +1,3 @@
+
+const { add, subtract, multiply, divide } = require("./index");
+console.log(add(2,2));
